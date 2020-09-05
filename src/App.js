04209e502c5fb19b532862d48f05c39757abb112
Dan <daniel.murphy.1@gmail.com>
2020-09-05 11:51:50 -0400

    query second planets page

--- src/App.js
+++ src/App.js
@@ -15,30 +15,46 @@ class App extends React.Component {
       characters:[], 
       planets: []
     }
+
+    
   }
 
   componentDidMount(){
+    const planetsArray = [];
     this.setState({loading: true})
     fetch("https://swapi.dev/api/people/")
       .then(response => response.json())
       .then(data => {
-        fetch("http://swapi.dev/api/planets/")
-        .then(response2 => response2.json())
-        .then(data2 => {
-          this.setState({
-            loading: false,
-            characters: data.results,
-            planets: data2.results
-          })
+        fetch("http://swapi.dev/api/planets/?page=1")
+          .then(response2 => response2.json())
+          .then(planets1 => {
+            planetsArray.push(planets1.results);
+            console.log(planetsArray)
+            fetch("http://swapi.dev/api/planets/?page=2")
+            .then(response3 => response3.json())
+            .then(planets2 => {
+              planetsArray.push(planets2.results);
+
+              console.log(planetsArray);
+              console.log(planetsArray[0][0].name);
+          
+            this.setState({
+              loading: false,
+              characters: data.results,
+              planets: planetsArray
+            })
+            console.log(this.state.planets);
+        })
       
-          console.log(data)
-          console.log(data2)
+          //console.log(data)
       })
     })
 
     .catch(function(){
       alert("error")
     })
+
+    
   }
     render() {
       return (
